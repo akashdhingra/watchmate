348d3da3313513476b17e72a9cff83904e1abe49
Akash Dhingra <akashdhingra1996@gmail.com>
2024-03-10 14:39:01 -0400

    Added views

--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -8,3 +8,12 @@ def movie_list(request):
     movies = Movie.objects.all()
     data = {'movie' : list(movies.values())}
     return JsonResponse(data)
+
+def movie_details(request, pk):
+    movie = Movie.objects.get(id=pk)
+    data = {
+        'name' : movie.name,
+        'description' : movie.description,
+        'active' : movie.active
+        }
+    return JsonResponse(data)
