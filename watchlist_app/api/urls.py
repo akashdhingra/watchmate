72d9faca084dd98620eadd6efd0b5917fafd2d3c
Akash Dhingra <akashdhingra1996@gmail.com>
2024-03-11 13:39:00 -0400

    Implemented APIClass view

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from watchlist_app.api.views import movie_details, movie_list
+# from watchlist_app.api.views import movie_details, movie_list
+from watchlist_app.api.views import MovieListAV, MovieDetail
 
 
 urlpatterns = [
-    path('list/', movie_list, name='movie-list'),
-    path('<int:pk>/', movie_details, name='movie-details'),
+    path('list/', MovieListAV.as_view(), name='movie-list'),
+    path('<int:pk>/', MovieDetail.as_view(), name='movie-details'),
 ]
