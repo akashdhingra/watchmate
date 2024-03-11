d2118af4b61cf114a639d758f288ca864ae8e81f
Akash Dhingra <akashdhingra1996@gmail.com>
2024-03-11 13:20:05 -0400

    Added status codes for all responses

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -2,6 +2,7 @@ from watchlist_app.api.serializers import MovieSerializer
 from watchlist_app.models import Movie
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 @api_view(['GET','POST'])
 def movie_list(request):
@@ -24,7 +25,11 @@ def movie_list(request):
 def movie_details(request, pk):
     
     if request.method == "GET":
-        movie = Movie.objects.get(pk=pk)
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({'Error' : 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
+            
         serializer = MovieSerializer(movie)
         return Response(serializer.data)
     
@@ -35,11 +40,10 @@ def movie_details(request, pk):
             serializer.save()
             return Response(serializer.data)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
             
         
-    # if request.method == "DELETE":
-    #     movie = Movie.objects.get(pk=pk)
-    #     serializer = MovieSerializer(movie)
-    #     movie.delete()
-    #     return Response(serializer.data)
+    if request.method == "DELETE":
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
