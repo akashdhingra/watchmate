1564db2367dcc62e6248b75daedd8ccbf9e9b699
Akash Dhingra <akashdhingra1996@gmail.com>
2024-03-10 20:54:33 -0400

    separate API folder and implemented GET Serializer

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from watchlist_app.views import movie_details, movie_list
+from watchlist_app.api.views import movie_details, movie_list
 
 
 urlpatterns = [
--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,19 +1,19 @@
-from django.http import JsonResponse
-from django.shortcuts import render
+# from django.http import JsonResponse
+# from django.shortcuts import render
 
-from watchlist_app.models import Movie
+# from watchlist_app.models import Movie
 
-# Create your views here.
-def movie_list(request):
-    movies = Movie.objects.all()
-    data = {'movie' : list(movies.values())}
-    return JsonResponse(data)
+# # Create your views here.
+# def movie_list(request):
+#     movies = Movie.objects.all()
+#     data = {'movie' : list(movies.values())}
+#     return JsonResponse(data)
 
-def movie_details(request, pk):
-    movie = Movie.objects.get(id=pk)
-    data = {
-        'name' : movie.name,
-        'description' : movie.description,
-        'active' : movie.active
-        }
-    return JsonResponse(data)
+# def movie_details(request, pk):
+#     movie = Movie.objects.get(id=pk)
+#     data = {
+#         'name' : movie.name,
+#         'description' : movie.description,
+#         'active' : movie.active
+#         }
+#     return JsonResponse(data)
