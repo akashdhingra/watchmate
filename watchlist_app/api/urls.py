09d62b6da7bb6ad591f260415edd52bd29bc1778
Akash Dhingra <akashdhingra1996@gmail.com>
2024-03-11 21:19:04 -0400

    Added view for stream id's

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,12 +1,12 @@
 
 from django.urls import path
 # from watchlist_app.api.views import movie_details, movie_list
-from watchlist_app.api.views import WatchListAV, MovieDetail, StreamPlatformAV
+from watchlist_app.api.views import WatchListAV, MovieDetail, StreamPlatformAV, StreamDetail
 
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'),
     path('<int:pk>/', MovieDetail.as_view(), name='movie-details'),
     path('stream/', StreamPlatformAV.as_view(), name='stream'),
-    # path('stream/<int:pk>/', StreamDetail.as_view(), name='stream-details'),
+    path('stream/<int:pk>/', StreamDetail.as_view(), name='stream-details'),
 ]
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -65,7 +65,30 @@ class MovieDetail(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
         
 # Will implemented later
-# class StreamDetail(APIView):
+class StreamDetail(APIView):
+    def get(self, request, pk):
+        try:
+            stream = StreamPlatform.objects.get(pk=pk)
+        except StreamPlatform.DoesNotExist:
+            return Response({'Error' : 'Not found'}, status=status.HTTP_404_NOT_FOUND)
+            
+        serializer = StreamPlatformSerializer(stream)
+        return Response(serializer.data)
+        
+    
+    def put(self, request, pk):
+        stream = StreamPlatform.objects.get(pk=pk)
+        serializer = StreamPlatformSerializer(stream, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self, request, pk):
+        stream = StreamPlatform.objects.get(pk=pk)
+        stream.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
     
             
     
