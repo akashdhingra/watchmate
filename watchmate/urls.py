81875870aa94c0798e036c5262930ba5c6e07345
Akash Dhingra <akashdhingra1996@gmail.com>
2024-03-18 19:44:34 -0400

    Added temporary login and logout

--- watchmate/urls.py
+++ watchmate/urls.py
@@ -22,4 +22,5 @@ import watchlist_app
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('watch/', include('watchlist_app.api.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
