481ba2daa79be5b5203023ebf38f525155717581
Akash Dhingra <akashdhingra1996@gmail.com>
2024-03-23 23:00:54 -0400

    APITesting : Logout and Login

--- user_app/tests.py
+++ user_app/tests.py
@@ -8,10 +8,29 @@ class RegisterTestCase(APITestCase):
     
     def test_register(self):
         data ={
-            "username":"test",
+            "username":"example",
             "email":"test@gmail.com",
             "password":"password",
             "password2":"password"
         }
         response = self.client.post(reverse('register'), data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+class LoginLogoutTestCase(APITestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create_user(username="example", password="password")
+        
+    def test_login(self):
+        data= {
+            "username":"example",
+            "password":"password"
+        }
+        response = self.client.post(reverse('login'),data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_logout(self):
+        self.token = Token.objects.get(user__username="example")
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
