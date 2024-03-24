8e5bf850027853c84f5898b74d4c848b29841517
Akash Dhingra <akashdhingra1996@gmail.com>
2024-03-24 12:51:19 -0400

    Streamplatform post testing

--- watchlist_app/tests.py
+++ watchlist_app/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+from watchlist_app.api import serializers
+from watchlist_app import models
 
-# Create your tests here.
+class StreamPlatformTestCase(APITestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create_user(username="example", password="password")
+        self.token = Token.objects.get(user__username=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        
+    def test_streamplatform_create(self):
+        data = {
+            "name" : "netflix",
+            "about" : "#1 Streaming platform",
+            "website" : "http://netflix.com"
+        }
+        
+        response = self.client.post(reverse('streamplatform-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
