92f7577dc11c3f858b7ce1948758dd259e228175
Akash Dhingra <akashdhingra1996@gmail.com>
2024-03-23 22:40:22 -0400

    APITesting : Register

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -6,7 +6,7 @@ from user_app.api.views import logout_view, registration_view
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
     path('register/', registration_view, name='register'),
-    path('logout/', logout_view, name='register'),
+    path('logout/', logout_view, name='logout'),
     
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
