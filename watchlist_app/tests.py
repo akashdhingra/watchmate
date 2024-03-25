2ee93803c86c81549b3cef4ca8a1a2c2ea283062
Akash Dhingra <akashdhingra1996@gmail.com>
2024-03-24 21:31:36 -0400

    API Testing : WatchList

--- watchlist_app/tests.py
+++ watchlist_app/tests.py
@@ -34,3 +34,37 @@ class StreamPlatformTestCase(APITestCase):
     def test_streamplatform_ind(self):
         response = self.client.get(reverse('streamplatform-detail', args = (self.stream.id,)))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+
+class WatchListTestCase(APITestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create_user(username="example", password="password")
+        self.token = Token.objects.get(user__username=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        self.stream = models.StreamPlatform.objects.create(name = "Netflix", description = "#1 Platform",
+                                                           website = "www.netflix.com")
+        
+        self.watchlist = models.WatchList.objects.create(platform= self.stream, title = "Example",
+                                                         storyline = "Example storyline", active = True)
+    
+    def test_watchlist_create(self):
+        data = {
+            "title" : "Example movie",
+            "storyline" : "Example story",
+            "platform" : self.stream,
+            "active" : True    
+        }
+        
+        response = self.client.post(reverse('movie-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        
+    def test_watchlist_list(self):
+        response = self.client.get(reverse('watch-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_watchlist_ind(self):
+        response = self.client.get(reverse('movie-details', args=(self.watchlist.id,)))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(models.WatchList.objects.count(),1)
+        self.assertEqual(models.WatchList.objects.get(id=1).storyline,'Example storyline')
